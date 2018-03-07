23b44de4b56b9d42c3ff3959d5988f4e7df87104
ITE152A-37@M307.uc-bcf.edu.ph <ITE152A-37@M307.uc-bcf.edu.ph>
2018-03-07 15:34:41 +0800

    joomla

---
+++ Joomla/administrator/logs/error.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-02-26 08:03:40 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-02-26T08:03:40+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-26T08:03:42+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-28T07:12:42+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-28T07:40:07+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-02-28T07:59:08+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-03-02T07:05:25+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-03-02T08:02:28+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
